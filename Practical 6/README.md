09b27c153e30465b502c38516894d6e6455785cd
RaghulRavikumar <raghul.ravikumar@ucdconnect.ie>
2021-11-11 22:57:48 +0000

    code for practical 6

---
+++ Practical 6/README.md
@@ -0,0 +1,15 @@
+# PRACTICAL 6
+
+## matrixmultiplication.c
+
+- C program to multiply matrix A and matrix B
+
+## Step6 -> main.c
+
+- C program that consist of function which is in matmult.c 
+
+## matmult.c
+
+- C program that consist of matrix multiplication function only
+
+
---
+++ Practical 6/matrixmultiplication.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+
+void main()
+{
+    int n=5,p=3,q=4; //initializing size of the matrix
+    double A[n][p],B[p][q],C[n][q]; //declaring matrix
+    int i,j,k;
+    //initializing matrix C
+    for (i = 0; i < n; i++)
+    for (j = 0; j < q; j++)
+        C[i][j]=0.0;
+
+    //initializing matrix A
+    for (i = 0; i < n; i++)
+    for (j = 0; j < p; j++)
+        A[i][j]=i+j;
+    
+    //initializing matrix B
+    for (i = 0; i < p; i++)
+    for (j = 0; j < q; j++)
+        B[i][j]=i-j;
+    
+    //matrix multiplication
+    for (i = 0; i < n; i++)
+    for (j = 0; j < q; j++)
+    for (k = 0; k < p; k++)
+        C[i][j]=C[i][j]+A[i][k]*B[k][j];
+    
+    //Printing Matrix A
+    printf("\n This is Matrix A:\n\n");
+    for (i = 0; i < n; i++)
+    {
+    for (j = 0; j < p; j++)
+    printf("%3.0f ",A[i][j]);
+    printf("\n");  
+    }
+
+    //Printing Matrix B
+    printf("\n This is Matrix B:\n\n");
+    for (i = 0; i < p; i++)
+    {
+    for (j = 0; j < q; j++)
+    printf("%3.0f ",B[i][j]);
+    printf("\n");  
+    }
+
+    //Printing Matrix C
+    printf("\n This is Matrix C:\n\n");
+    for (i = 0; i < n; i++)
+    {
+    for (j = 0; j < q; j++)
+    printf("%3.0f ",C[i][j]);
+    printf("\n");  
+    }
+}
